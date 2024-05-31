6a0b636b24e0875585bb6844da757e80a4efee74
Flavio Mesquita Marinho Filho <flaviomesquitamarinho13@gmail.com>
2024-05-30 23:42:26 -0300

    Criado projeto com ASP.NET CORE

---
+++ ProjetoReservaHotel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoReservaHotel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
